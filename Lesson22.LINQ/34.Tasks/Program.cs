ac34b360fc386441620d52a02656e58803264d32
fchuseynli <hseynli@list.ru>
2023-07-04 15:18:22 +0400

    Added tasks

---
+++ Lesson22.LINQ/34.Tasks/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+1. id-si 0-a bölünən Personları 
+2. id-si 0-100 aralığında olan Personları
+3. sonu a ilə bitən Personları
+4. Adı 7 simvoldan ibarət olan Personları
+5. Adı A və  ya Z ilə başlayan Personları
+6. Doğum tarixi 01.01.2000-ci ildən böyük olan Personları
+7. Adı B ilə başlayan ilk 10 Personu
+8. Doğum tarixi ən böyük olan Personu
+9. Doğum tarixi ən böyük olandan bir vahid az olan Personu
+10. Anonim tip ilə FirstName və LastName ilə birləşdirin və FullName yaradın
+11. Yuxarıdakı taskı PersonFullName klası ilə edin. Klasın içində FullName propertysi olacaqdır.
+12. Doğum tarixi 01.01.2000-ci ildən böyük olan Personların əmək haqqısının cəmini toplayın. Salary property-si yoxdursa onu yaradın.
+13. 0-1000 aralığında ədədlər verilib. LINQ ilə hər bir ədədin kvadratını tapın (loopsuz)
+14. Eyni adların sayını tapın
+15. Hər bir adın birinci simvolunu tapın (LINQ)
+16. Hər bir verillən ad və soyadı böyük registrə çevirin
+17. Ən az simvoldan ibarət olan adı tapın
+18. Hər 10-cu Personu ekrana çıxarın
+19. Doğum ayı Sentyabr olan Personları seçin
+20. Personun adında iki `a` hərfi olanları seçin
+BÜTÜN TAPŞIRIQLARI HƏM LAMBDA EXPRESSİON, HƏM DƏ SQL LİKE SYNTAX İLƏ YAZIN
+ */
+
+Console.WriteLine("\nDone!");
